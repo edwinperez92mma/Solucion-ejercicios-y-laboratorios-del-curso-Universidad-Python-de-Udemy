94113f83a4d20e7eda6315e3412157c3f4fe2eed
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 21:49:00 -0500

    Ejercicio_para_calcular_si_un_numero_es_par_o_no

---
+++ 6_numero_par_o_impar.py
@@ -0,0 +1,9 @@
+print("Número par o impar - ejercicio")
+#Escribir un programa que permita determinar si un número introducido por el usuario, es o no par
+
+numero = int(input("Digite el número a analizar: "))
+
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
