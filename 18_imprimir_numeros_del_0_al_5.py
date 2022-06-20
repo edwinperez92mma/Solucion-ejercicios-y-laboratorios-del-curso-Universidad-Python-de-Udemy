af465504fada6c79c2046471374f5f06e7411e65
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 21:58:48 -0500

    Ejercicio_ciclos

---
+++ 18_imprimir_numeros_del_0_al_5.py
@@ -0,0 +1,19 @@
+print("Imprimir números del 0 al 5 - ejercicio")
+#Imprimir los números enteros del 0 al 5 utilizando un ciclo while
+
+"""
+contador = 0
+
+while contador <= 5:
+    print(contador)
+    contador += 1
+"""
+
+print("Solución propuesta por el instructor")
+
+maximo = 5
+contador = 0
+
+while contador <= maximo:
+    print(contador)
+    contador += 1
