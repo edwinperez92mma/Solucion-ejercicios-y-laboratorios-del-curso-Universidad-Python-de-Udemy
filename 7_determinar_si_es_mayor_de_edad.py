657d52f187475b071d07f3ca218541c6ce6eee50
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 21:50:50 -0500

    Determinar_si_un_usuario_es_mayor_de_edad

---
+++ 7_determinar_si_es_mayor_de_edad.py
@@ -0,0 +1,9 @@
+print("Determinar si es mayor de edad - ejercicio")
+#Preguntar la edad de la persona, e imprimir un mensaje en pantalla informando si es o no mayor de edad
+
+edad = int(input("¿Cuál es su edad?: "))
+
+if edad >= 18:
+    print("Sí es mayor de edad")
+else:
+    print("No es mayor de edad")
