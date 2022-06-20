dd9654f20599ddf54c86c42177782eca7ae984f9
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 21:39:48 -0500

    Ejercicio_de_entrada_y_salida_de_datos

---
+++ detalles_de_un_libro.py
@@ -0,0 +1,12 @@
+print("Detalles de un libro - ejercicio")
+#Se solicita incluir la siguiente información acerca de un libro:
+#Título
+#Autor
+#Debes imprimir la información en el siguiente formato:
+# Proporciona el título:
+# Proporciona el autor:
+# <título> fue escrito por <autor>
+
+titulo = input("Proporciona el título: ")
+autor = input("Proporciona el autor: ")
+print(titulo, "fue escrito por", autor)
