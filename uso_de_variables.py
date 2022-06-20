4afa306765b62a7ec0e3d62aa664b52f27d40b7d
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 21:35:21 -0500

    Ejercicio_de_uso_de_variables

---
+++ uso_de_variables.py
@@ -0,0 +1,14 @@
+print("Uso de variables - ejercicio")
+#Crear 3 variables a las que se les asignarán los siguientes valores para luego mostrarlos en pantalla:
+#Edwin Perez
+#5126700
+#edwinperez92mma@gmail.com
+
+#Declaración de variables
+nombre = "Edwin Perez"
+telefono = 5126700
+email = "edwinperez92mma@gmail.com"
+
+print(nombre)
+print(telefono)
+print(email)
