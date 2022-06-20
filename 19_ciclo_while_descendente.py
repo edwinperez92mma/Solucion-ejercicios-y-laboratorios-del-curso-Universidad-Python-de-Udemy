831d9d033899861956aeb80dc76f11e649dd3181
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 22:00:09 -0500

    Ejercicio_ciclos

---
+++ 19_ciclo_while_descendente.py
@@ -0,0 +1,19 @@
+print("Ciclo while descendente")
+#Imprimir números del 5 al 1 de manera descendente
+
+"""
+contador = 5
+
+while contador >= 1:
+    print(contador)
+    contador -= 1
+"""
+
+print("Solución propuesta por el instructor")
+
+minimo = 1
+contador = 5
+
+while contador >= minimo:
+    print(contador)
+    contador -= 1
