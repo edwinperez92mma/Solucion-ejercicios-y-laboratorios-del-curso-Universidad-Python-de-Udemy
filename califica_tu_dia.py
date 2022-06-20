a59bc25fea9fab0622722b0194543d889b818823
Edwin Joan Perez Benitez <75395175+edwinperez92mma@users.noreply.github.com>
2022-06-19 21:38:18 -0500

    Ejercicio_de_entrada_y_salida_de_datos

---
+++ califica_tu_dia.py
@@ -0,0 +1,5 @@
+print("Califica tu día - ejercicio")
+#Califica tu día del 1 al 10, y que devuelva el mensaje "Mi día estuvo de 10"
+
+calificacion = int(input("Califica tu día del 1 al 10: "))
+print("Mi día estuvo de", calificacion)
